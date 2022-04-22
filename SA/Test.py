650ecd69a8fc7860868dbead96bb72b6845e9a3b
Aprigio4 <aprigio.malveiro@tecnico.ulisboa.pt>
2022-04-22 10:33:12 +0100

    Update Test.py

--- SA/Test.py
+++ SA/Test.py
@@ -1,4 +1,4 @@
-from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, Reshape, Dense, multiply, Permute, Concatenate, Conv2D, Add, Activation, Lambda
+from keras.layers import GlobalAveragePooling3D, GlobalMaxPooling3D, Reshape, Dense, multiply, Permute, Concatenate, Conv3D, Add, Activation, Lambda
 from keras import backend as K
 from keras.activations import sigmoid
 
@@ -8,18 +8,18 @@ def spatial_attention(input_feature):
 	
 	if K.image_data_format() == "channels_first":
 		channel = input_feature.shape[1]
-		cbam_feature = Permute((2,3,1))(input_feature)
+		cbam_feature = Permute((2,3,4,1))(input_feature)
 	else:
 		channel = input_feature.shape[-1]
 		cbam_feature = input_feature
 	
-	avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+	avg_pool = Lambda(lambda x: K.mean(x, axis=4, keepdims=True))(cbam_feature)
 	assert avg_pool.shape[-1] == 1
-	max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+	max_pool = Lambda(lambda x: K.max(x, axis=4, keepdims=True))(cbam_feature)
 	assert max_pool.shape[-1] == 1
-	concat = Concatenate(axis=3)([avg_pool, max_pool])
+	concat = Concatenate(axis=4)([avg_pool, max_pool])
 	assert concat.shape[-1] == 2
-	cbam_feature = Conv2D(filters = 1,
+	cbam_feature = Conv3D(filters = 1,
 					kernel_size=kernel_size,
 					strides=1,
 					padding='same',
@@ -29,6 +29,6 @@ def spatial_attention(input_feature):
 	assert cbam_feature.shape[-1] == 1
 	
 	if K.image_data_format() == "channels_first":
-		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+		cbam_feature = Permute((4, 1, 2, 3))(cbam_feature)
 		
 	return multiply([input_feature, cbam_feature])
